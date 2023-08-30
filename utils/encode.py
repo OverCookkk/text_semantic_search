8830828aaba94a9c999a1562ac972f45f320d093
OverCookkk <543701203@qq.com>
2023-08-30 18:26:16 +0800

    修改README

--- utils/encode.py
+++ utils/encode.py
@@ -13,11 +13,9 @@ from config import MODEL_PATH
 
 class SentenceModel:
     """
-    Say something about the ExampleCalass...
-
-    Args:
-        args_0 (`type`):
-        ...
+    paraphrase-mpnet-base-v2是基于BERT（Bidirectional Encoder Representations from Transformers）的模型，用于进行句子重述（paraphrasing）任务。它是通过在大规模文本语料库上进行预训练而得到的，具有深度的双向Transformer结构。
+    该模型的目标是将输入的句子转化为与原始句子意思相似但表达方式不同的句子。它可以用于多种自然语言处理任务，如问答系统、文本摘要、机器翻译等，以及一些应用领域，如信息检索和文本生成。
+    paraphrase-mpnet-base-v2模型在预训练阶段使用了大量的无监督数据，通过学习上下文关系和语义表示来捕捉句子之间的相似性。在具体任务中，可以通过微调（fine-tuning）这个模型来适应特定的句子重述任务，并提供更准确的结果。
     """
 
     def __init__(self):
